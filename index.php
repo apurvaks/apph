f1405ef158e5bd5ca62bc4756a4867f8383c439c
apurvaks <kumarapurva007@gmail.com>
2017-03-15 02:12:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ select{
 
 <?php 
 
- if(isset($_POST['submit'])){
+ 
         function pg_connection_string_from_database_url() {
          extract(parse_url($_ENV["DATABASE_URL"]));
          return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
@@ -159,5 +159,5 @@ select{
    
     pg_close();
 
-   }
+   
         ?> 
